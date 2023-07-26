0589c31c4f80386acf52641aa6dd75d5acfc5033
Chenghsuan Ho <chengxuanjhe@gmail.com>
2023-07-27 00:04:55 +0800

    feat(metarole): 列表加上同步時間

--- src/commands/metarole.ts
+++ src/commands/metarole.ts
@@ -166,9 +166,15 @@ const command: Command = {
         for (const m of metaroles) {
           const memberRoles = m.memberRoles.map((r) => `<@&${r}>`).join(", ");
           const metaroleSize = roles?.get(m.role.toString())?.members.size;
+          const syncTimestamp = m.syncedAt
+            ? Math.floor(m.syncedAt.getTime() / 1000)
+            : null;
+          const syncStatusMessage = syncTimestamp
+            ? `<t:${syncTimestamp}:R>同步過`
+            : "尚未同步";
 
           replyContent.push(
-            `<@&${m.role}>：包含 ${memberRoles}，共 ${metaroleSize} 位成員\n`,
+            `<@&${m.role}>：包含 ${memberRoles}，共 ${metaroleSize} 位成員，${syncStatusMessage}。\n`,
           );
         }
 
